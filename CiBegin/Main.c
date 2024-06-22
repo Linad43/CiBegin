68f9fa34585d2032ce85f7c175e6fe34f5479f75
Linad <merzlikindaniil97@gmail.com>
2024-06-22 10:59:39 +0300

    Добавьте файлы проекта.

---
+++ CiBegin/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "TransfFarToCels.h"
+
+main()
+{
+	setlocale(0, "");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	printf("Здравствуй мир!\n");
+
+	TransfFarToCels();
+}
---
+++ CiBegin/TransfFarToCels.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define BEG 0
+#define END 100
+#define STEP 10
+
+TransfFarToCels() {
+	float fahr, celsius;
+	printf("Фаренгейт Цельсий\n");
+	for (fahr = BEG; fahr <= END; fahr += STEP) {
+		celsius = (5.0 / 9.0) * (fahr - 32);
+		printf("%9.2f %7.2f\n", fahr, celsius);
+	}
+	printf("\n");
+
+	printf("Цельсий Фарегнейт\n");
+	for (celsius = BEG; celsius <= END; celsius += STEP) {
+		fahr = (celsius * (9.0 / 5.0)) + 32;
+		printf("%7.2f %9.2f\n", celsius, fahr);
+	}
+	printf("\n");
+}
